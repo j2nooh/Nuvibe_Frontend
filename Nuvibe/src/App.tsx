ebb59d97087a140f182b4f39b7e43714c902561a
goody <ehdgusqaz98@gmail.com>
2025-12-25 00:52:53 +0900

    초기설정

--- Nuvibe/src/App.tsx
+++ Nuvibe/src/App.tsx
@@ -4,6 +4,12 @@ function App() {
 
   return (
     <>
+      <div className="relative flex flex-col w-full min-h-[100dvh]">
+      {/* 중앙 컨텐츠 영역 */}
+        <main className="flex items-center justify-center">
+          <h1 className="H0 text-white">Nuvibe</h1>
+        </main>
+    </div>
     </>
   )
 }
